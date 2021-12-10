578e70d65c495cd53d7c1c53e94c224485300851
devexpert127 <mlobovic@gmail.com>
2021-12-10 12:38:17 +0300

    fix issue on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { getSolflareWallet, getSolletWallet } from '@solana/wallet-adapter-wallets';
-import { getPhantomWallet, getSlopeWallet } from '@solana/wallet-adapter-wallets';
-import { useMemo } from 'react';
 import './App.less';
 import { OptionMarket } from './components/OptionMarket';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { ConnectionProvider } from '@solana/wallet-adapter-react';
 
 function App() {
-  // const network = useRecoilValue(atomNetwork);
-
-  const wallets = useMemo(
-    () => [
-      getPhantomWallet(),
-      getSolflareWallet(),
-      getSlopeWallet(),
-      getSolletWallet(),
-    ], []
-  );
   
   return (
     <ConnectionProvider endpoint="http://127.0.0.1:8899">
-    <WalletProvider autoConnect wallets={wallets}>
-
     <div className="App">
       <OptionMarket/>
     </div>
-    </WalletProvider>
     </ConnectionProvider>
 
   );
